bf35560474d6cc60b72a8d669bf368a47414bfc0
Hatchi-Kin <j.needham@hotmail.fr>
2024-09-03 01:07:08 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from fasthtml import FastHTML
+from fasthtml.common import Div, H1, H2, P, Title, Main, Link, Style, A, Button, Details, Summary, Script, Img, picocss
+from starlette.responses import FileResponse
+import uvicorn
+
+from repos import top_repos
+
+
+picolink = (Link(rel="stylesheet", href=picocss), Style(":root { --pico-font-size: 100%; }"))
+sortablejs = Script(src="https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.14.0/Sortable.min.js")
+
+app = FastHTML(hdrs=(picolink, sortablejs), static_dir='static')
+
+
+@app.get("/static/{fname}")
+def static(fname: str):
+    return FileResponse(f'static/{fname}')
+
+def create_repo_card(repo):
+    img_element = Img(src=f"/static/{repo['preview']}", alt=f"{repo['name']} image", style="margin-top: 12px;") if 'preview' in repo else None
+
+    return Details(
+        Summary(H2(repo['name'], style="color: black;")),
+        Div(
+            Div(
+                P(repo['description'], style="color: black;"),
+                cls="div1",
+                style="grid-area: 1 / 1 / 2 / 4;"
+            ),
+            Div(
+                A(Button("Go to Repo", cls="button"), href=repo['url']),
+                cls="div2",
+                style="grid-area: 1 / 4 / 2 / 5; display: flex; justify-content: flex-end; align-items: center; align-self: center; margin-top: -10px;"
+            ),
+            cls="parent",
+            style="display: grid; grid-template-columns: repeat(4, 1fr); grid-template-rows: 1fr; grid-column-gap: 0px; grid-row-gap: 0px;"
+        ),
+        img_element,
+        style=f"background-color: {repo['color']}; margin: 12px; padding: 12px; border-radius: 8px; max-width: 700px;"
+    )
+
+
+@app.get("/")
+def home():
+    all_cards = [create_repo_card(repo) for repo in top_repos]
+    return Title("Hatchi-Kin's repositories"), Main(
+        H1("Hatchi-Kin's selection of fresh repos !", style="margin-bottom: 20px; margin-top: 20px;"),
+        Div(
+            Img(src="/static/github_preview.webp", alt="Hatchi-Kin logo", style="width: 100%; height: auto;"),
+            style="max-width: 700px; margin: 0 auto;"
+        ),
+        Div(*all_cards, id="card-container", style="margin-top: 20px;"),
+        Div(
+            "Load More",
+            hx_get="/show-more",
+            hx_trigger="click",
+            hx_swap="outerHTML",
+            id="load-more",
+            cls="button",
+            style="margin-top: 20px; cursor: pointer;"
+        ),
+        Script("""
+            document.addEventListener('DOMContentLoaded', function() {
+                // Hide cards beyond the first 5
+                const cards = document.querySelectorAll('#card-container > details');
+                for (let i = 5; i < cards.length; i++) {
+                    cards[i].style.display = 'none';
+                }
+
+                // Initialize Sortable
+                var el = document.getElementById('card-container');
+                Sortable.create(el, {
+                    animation: 150,
+                    ghostClass: 'sortable-ghost'
+                });
+
+                // Function to show more cards
+                function showMoreCards() {
+                    const hiddenCards = document.querySelectorAll('#card-container > details[style*="display: none"]');
+                    hiddenCards.forEach(card => card.style.display = '');
+                    document.getElementById('load-more').remove();
+                }
+
+                // Attach click event to load more button
+                document.getElementById('load-more').addEventListener('click', showMoreCards);
+            });
+        """),
+        style="max-width: 800px; margin: 0 auto;"
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run("app:app", host='0.0.0.0', port=8004, reload=True)
---
+++ repos.py
@@ -0,0 +1,83 @@
+megapi = {
+    "name": "MegaPi",
+    "description": "FastAPI Backend for the music-sim app.",
+    "url": "https://github.com/Hatchi-Kin/megapi",
+    "color": "#5d77aa",
+    "preview": "megapi_preview.webp",
+}
+
+music_sim = {
+    "name": "MusicSim",
+    "description": "React frontend for music-sim app.",
+    "url": "https://github.com/Hatchi-Kin/react-music-sim",
+    "color": "#6a83b3",
+    "preview": "react-music-sim_preview.webp",
+}
+
+monitoring_stack = {
+    "name": "Monitoring Stack",
+    "description": "Monitoring stack for music-sim app.",
+    "url": "https://github.com/Hatchi-Kin/monitoring-stack",
+    "color": "#778fbc",
+    "preview": "monitoring-stack_preview.webp",
+}
+
+quatuomotron = {
+    "name": "Quatuomotron",
+    "description": "A simple terminal app to generate random groups.",
+    "url": "https://github.com/Hatchi-Kin/quatuomotron",
+    "color": "#849bc5",
+    "preview": "quatuomotron_preview.webp",
+}
+
+binomotron = {
+    "name": "Binomotron",
+    "description": "A simple terminal app to generate random groups. This is the first practical excercise of the SIMPLON bootcamp for AI development.",
+    "url": "https://github.com/Hatchi-Kin/binomotron",
+    "color": "#91a7ce",
+    "preview": "binomotron_preview.webp",
+}
+
+mlflow_setup = {
+    "name": "Quick MLflow Setup",
+    "description": "A simple template to speed up setting up MLflow.",
+    "url": "    https://github.com/Hatchi-Kin/quick_mlflow_setup",
+    "color": "#d1d8e7",
+}
+
+py_traducteur = {
+    "name": "PyTraducteur",
+    "description": "A translator app with a bugged solved.",
+    "url": "https://github.com/Hatchi-Kin/py-traducteur",
+    "color": "#d1d8e7",
+}
+
+streamtanic = {
+    "name": "Streamtanic",
+    "description": "A simple streamlit app / Titanic Kaggle model.",
+    "url": "https://github.com/Hatchi-Kin/streamtanic",
+    "color": "#d1d8e7",
+}
+
+music_tous_la = {
+    "name": "Music Tous La",
+    "description": "A Slack Bot for the 'music tous la' ritual.",
+    "url": "https://github.com/Hatchi-Kin/MusicTousLa",
+    "color": "#d1d8e7",
+}
+
+roomba = {
+    "name": "Roomba",
+    "description": "A Discord bot that lets an authenticated user check their calendar.",
+    "url": "https://github.com/Hatchi-Kin/Room_Ba",
+    "color": "#d1d8e7",
+}
+
+clever_door = {
+    "name": "Clever Door",
+    "description": "A face recognition app to learn about training Vision AI models.",
+    "url": "https://github.com/Hatchi-Kin/Clever_Door",
+    "color": "#d1d8e7",
+}
+
+top_repos = [megapi, music_sim, monitoring_stack, quatuomotron,binomotron, mlflow_setup, py_traducteur, streamtanic, music_tous_la, roomba, clever_door]
